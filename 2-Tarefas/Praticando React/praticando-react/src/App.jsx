06f0dee205b8a856b91a241e6116666d2f9587d5
GustaHxH <gu.stavosantos8259@gmail.com>
2025-10-24 18:26:46 -0300

    final da seção
    acabei a seção do curso

---
+++ 2-Tarefas/Praticando React/praticando-react/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Teste 1 React</h1>
+      <p>Este é meu primeiro teste usando o react...</p>
+    </>
+  )
+}
+
+export default App
