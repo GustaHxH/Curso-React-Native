106ccea5f70b7842fd5c8097acaedde7ee097e11
GustaHxH <gu.stavosantos8259@gmail.com>
2025-11-05 15:54:10 -0300

    Criação de Comentários
    Comentários dentro do Jsx

--- 2-Fundamentos/fundamentos/src/components/FirstComponent.jsx
+++ 2-Fundamentos/fundamentos/src/components/FirstComponent.jsx
@@ -1,8 +1,13 @@
+//arquivos de estilo
+
 const FisrtComponent =  () =>{
+    //Essa função faz isso TESTE
 
     return(
         <div>
+            {/*Comentario interno*/}
             <h1>Meu primeiro componente</h1>
+            <p className="Teste">Meu texto</p>
         </div>
     );
 };
